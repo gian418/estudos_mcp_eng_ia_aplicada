7b53eddf11272e8dda9664a40d05e6f36a6f3d1d
Giancarlo Haack Albano <gian418@gmail.com>
2026-04-15 09:05:19 -0300

    Add MongoDB healthcheck and integrate MongoDB tool in Docker setup; enhance agent and intent node processing

--- 01-multiple-mcp-tools-template/src/graph/nodes/intentNode.ts
+++ 01-multiple-mcp-tools-template/src/graph/nodes/intentNode.ts
@@ -1,18 +1,37 @@
 import { AIMessage } from 'langchain';
 import { OpenRouterService } from '../../services/openRouterService.ts';
 import type { GraphState } from '../state.ts';
+import { getSystemPrompt } from '../../prompts/v1/agentNode.ts';
+import { type IntentData, IntentSchema } from '../../prompts/v1/identifyIntent.ts';
+import { parse } from 'path';
 
 export function intentNode(openRouterService: OpenRouterService) {
     return async (state: GraphState): Promise<Partial<GraphState>> => {
         console.log('🧠 Intent node processing...');
         try {
             const rawQuestion = state.messages.at(-1)!.text as string;
+            const result = await openRouterService.generateStructured(
+                getSystemPrompt(),
+                rawQuestion,
+                IntentSchema
+            )
 
+            const parsed = result.data as IntentData
+            if(!parsed.intent || !parsed.fileType) {
+                console.log('!!! Missing intent or fileType parsed data:', parsed)
+                throw new Error('Invalid intent data')
+            }
+
+            parsed.fileName ??= `data.${parsed.fileType}`
+
+            console.log('📄 Extracted intent:', parsed.intent);
+            console.log('📄 File Type:', parsed.fileType);
+            console.log('📄 File name:', parsed.fileName);
 
             return {
-                intent: '',
-                fileContent: '{}',
-                fileName: 'report.json',
+                intent: parsed.intent,
+                fileContent: parsed.fileContent ?? "",
+                fileName: parsed.fileName,
             };
 
         } catch (error) {
--- 01-multiple-mcp-tools-template/src/services/mcpService.ts
+++ 01-multiple-mcp-tools-template/src/services/mcpService.ts
@@ -1,6 +1,19 @@
-export const getMCPTools = async () => {
+import { MultiServerMCPClient } from "@langchain/mcp-adapters";
+import { getMongoDbTool } from "../tools/mongoDbTool.ts";
 
+export const getMCPTools = async () => {
+  const client = new MultiServerMCPClient({
+    mcpServers: {
+      ...getMongoDbTool(),
+    },
+    onMessage: (log, source) => {
+      console.log(`[${source.server}] ${log.data}`)
+    }
+  })
+
+  const mcpTools = await client.getTools()
 
   return [
+    ...mcpTools
   ];
 };
